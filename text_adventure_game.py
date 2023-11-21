25511169d2d056a28a08299ff2fd8d00f9203e7e
Knezgamer <nazghul1011@gmail.com>
2023-11-21 20:01:36 +0100

    early access 1.0

--- text_adventure_game.py
+++
@@ -1,3 +0,0 @@
-character_name = input("What is your character's name: ")
-
-print(f"In the actient land of Eldoria, where magic flowed like a river and mythical creatures roamed freely, there lived a young sorcerer named {character_name}.")
